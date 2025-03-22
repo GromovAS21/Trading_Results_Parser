f655d00735042e4c528f973b28acaf8cd8718f1a
Anton Gromov <seed14112@yandex.ru>
2025-03-22 19:07:28 +0300

    [~] Добавление property для класса ExcelParser

--- services/excel_parsers.py
+++ services/excel_parsers.py
@@ -6,10 +6,14 @@ class ExcelParser:
 
     def __init__(self, url_excel):
         self._url_excel = url_excel
-        self.table = self.load_excel_file()
+        self._table = self.load_excel_file()
         self._rename_columns()
         self._clean_data()
         self._filter_data()
+
+    @property
+    def table(self):
+        return self._table
 
     def load_excel_file(self) -> pd.DataFrame:
         """Читает Excel-файл и возвращает DataFrame с нужными столбцами"""
@@ -17,22 +21,20 @@ class ExcelParser:
 
     def _rename_columns(self) -> None:
         """Переименовывает столбцы DataFrame."""
-        self.table.columns = ["код_инструмента", "наименование_инструмента", "базис_поставки",
+        self._table.columns = ["код_инструмента", "наименование_инструмента", "базис_поставки",
                                   "объем_договора в единицах измерения", "объем_договора", "количество_договоров"
                                   ]
 
     def _clean_data(self) -> None:
         """Очищает данные DataFrame от первой и последних двух строк(Итого, Итого по секции)"""
-        self.table = self.table.iloc[1:-2]
+        self._table = self.table.iloc[1:-2]
 
     def _filter_data(self) -> None:
         """Фильтрует данные DataFrame от строк, где количество договоров равно 0"""
-
-        self.table = self.table.loc[self.table["количество_договоров"].str.strip() != "-"]
+        self._table = self.table.loc[self.table["количество_договоров"].str.strip() != "-"]
 
 if __name__ == '__main__':
     url = "https://spimex.com/upload/reports/oil_xls/oil_xls_20250320162000.xls?r=9858"
     parser = ExcelParser(url)
     a = parser.table
-    for i, row in a.iterrows():
-        print(row["количество_договоров"])
+    print(a)
