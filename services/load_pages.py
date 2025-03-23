fc7e33a3599b4d614c8a8d0e2b68e987239883fb
Anton Gromov <seed14112@yandex.ru>
2025-03-24 01:29:56 +0300

    [~] Корректировка класса LoadPages
    R: Не работало переключения запроса на новую страницу
    
    FB:  Header не работал, был написан метод get_url_with_page и явно возвращает нужный Url

--- services/load_pages.py
+++ services/load_pages.py
@@ -2,13 +2,12 @@ import requests
 
 
 class LoadPages:
-    """Класс для загрузки страниц по указанному URL."""
+    """Класс для загрузки страниц по указанному URL"""
 
     def __init__(self, url_page: str):
-        self._url_page = url_page
-        self._headers = {}
+        self._base_url = url_page
+        self.headers = {}
         self._page_number = 1
-        self._headers["page"] = f"page-{self._page_number}"
 
     @property
     def page_number(self):
@@ -16,13 +15,16 @@ class LoadPages:
 
     @page_number.setter
     def page_number(self, number):
-        self._page_number += number
+        self._page_number = number
+
+    def get_url_with_page(self):
+        """Формирует URL с текущим номером страницы"""
+        return f"{self._base_url}?page=page-{self._page_number}"
 
     def download_html_page(self) -> str:
-        """Скачивает страницу по-указанному URL и возвращает её текст."""
-
+        """Скачивает страницу по-указанному URL и возвращает её текст"""
         try:
-            response = requests.get(self._url_page, headers=self._headers)
+            response = requests.get(self.get_url_with_page())
             return response.text
         except requests.exceptions.ConnectionError:
             raise ConnectionError("Указан неверный url")
