538ca7fc75c51a64513af505d7179125fd68385f
Anton Gromov <seed14112@yandex.ru>
2025-03-24 01:32:27 +0300

    [~] Изменен класс DataTransformation
    R: Выводилась ошибка из-за не правильной типизации которые записываются в словарь и не было проверки на корректность строки
    
    FB:  Добавлена проверка на корректность строки и сделано типизированние данных перед записью

--- services/data_transformation.py
+++ services/data_transformation.py
@@ -1,32 +1,43 @@
+import logging
 from typing import List
 
 import pandas as pd
+import sqlalchemy
 from pandas import DataFrame
+
+from models import TradingResults
 
 
 class DataTransformation:
     """Класс для преобразования данных"""
 
-    @staticmethod
-    def transform(data: DataFrame, date) -> List[dict]:
+    def transform(self, data: DataFrame, date) -> List[TradingResults]:
         """Преобразование данных из DataFrame в нужные колонки для базы данных"""
         data_for_db = []
         items = {}
         try:
             for idx, row in data.iterrows():
                 items['exchange_product_id'] = row['код_инструмента']
+                if "Итог" in items['exchange_product_id']:
+                    continue
                 items['exchange_product_name'] = row['наименование_инструмента']
                 items['oil_id'] = row['код_инструмента'][:4]
                 items['delivery_basis_id'] = row['код_инструмента'][4:7]
                 items['delivery_basis_name'] = row['базис_поставки']
                 items['delivery_type_id'] = row['код_инструмента'][-1]
-                items['volume'] = row['объем_договора в единицах измерения']
-                items['total'] = row['объем_договора']
-                items['count'] = row['количество_договоров']
+                items['volume'] = int(row['объем_договора в единицах измерения'])
+                items['total'] = float(row['объем_договора'])
+                items['count'] = int(row['количество_договоров'])
                 items['date'] = date
                 items['created_on'] = pd.to_datetime('now')
                 items['updated_on'] = pd.to_datetime('now')
-                data_for_db.append(items)
-        except TypeError:
+                trading_results_object = self._create_trading_results_object(items)
+                data_for_db.append(trading_results_object)
+        except (TypeError, ValueError):
             pass
         return data_for_db
+
+    @staticmethod
+    def _create_trading_results_object(data):
+        """Функция для создания объекта TradingResults"""
+        return TradingResults(**data)
