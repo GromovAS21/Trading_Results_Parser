31879aac6eca0f71a3a3af76e9a5694da0b2ce9e
Anton Gromov <seed14112@yandex.ru>
2025-04-04 12:49:53 +0300

    [+] Написание точки входа для запуска приложения

--- repositories.py
+++ repositories.py
@@ -5,7 +5,19 @@ class TradingResultsRepository:
     """Класс для работы с таблицей TradingResults в базе данных"""
 
     def __init__(self, session):
+        """
+        Инициализация репозитория.
+
+        Args:
+            session (Session): Объект сессии для работы с базой данных.
+        """
         self._session = session
 
     def add_all(self, results: list[TradingResults]):
-        return self._session.add_all(results)
+        """
+        Добавить все записи в базу данных.
+
+        Args:
+            results (list[TradingResults]): Список записей для добавления.
+        """
+        self._session.add_all(results)
