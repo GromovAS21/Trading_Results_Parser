2f336ad94cffcf70d9be184cd34b23e63442fb1e
Anton Gromov <seed14112@yandex.ru>
2025-04-04 09:29:19 +0300

    [-] Изменение файла excel_parsers.py
    R: Не было docstring`ов и не было фильрации по строке со значением "Итого:", "Итого по секции:"
    
    FB: Добавлена фильтрация по зачениям, написаны докстринги

--- services/excel_parsers.py
+++ services/excel_parsers.py
@@ -4,26 +4,51 @@ import pandas as pd
 class ExcelParser:
     """Класс для парсинга Excel-файлов."""
 
-    def __init__(self, url_excel):
-        self._url_excel = url_excel
-        self._table = self.load_excel_file()
+    def __init__(self, file_path):
+        """Инициализирует экземпляр класса.
+
+        Args:
+            file_path (str): Путь к Excel-файлу.
+        """
+        self.file_path = file_path
+        self._table = self.read_excel_file()
         self._rename_columns()
         self._filter_data()
 
     @property
     def table(self):
+        """
+        Возвращает DataFrame с нужными столбцами.
+
+        Returns:
+            pd.DataFrame: DataFrame с нужными столбцами.
+        """
         return self._table
 
-    def load_excel_file(self) -> pd.DataFrame:
-        """Читает Excel-файл и возвращает DataFrame с нужными столбцами"""
-        return pd.read_excel(self._url_excel, header=12, usecols=[1, 2, 3, 4, 5, 14])
+    def read_excel_file(self) -> pd.DataFrame:
+        """
+        Читает Excel-файл и возвращает DataFrame с нужными столбцами.
+
+        Returns:
+            pd.DataFrame: DataFrame с нужными столбцами.
+        """
+        return pd.read_excel(
+            self.file_path,
+            header=12,
+            usecols=[1, 2, 3, 4, 5, 14]
+        )
 
     def _rename_columns(self) -> None:
         """Переименовывает столбцы DataFrame."""
-        self._table.columns = ["код_инструмента", "наименование_инструмента", "базис_поставки",
-                               "объем_договора в единицах измерения", "объем_договора", "количество_договоров"
+        self._table.columns = ["код_инструмента",
+                               "наименование_инструмента",
+                               "базис_поставки",
+                               "объем_договора в единицах измерения",
+                               "объем_договора",
+                               "количество_договоров"
                                ]
 
     def _filter_data(self) -> None:
-        """Фильтрует данные DataFrame от строк, где количество договоров равно 0"""
-        self._table = self.table.loc[self.table["количество_договоров"].str.strip() != "-"]
+        """Фильтрует данные DataFrame."""
+        self._table = self.table.loc[(self.table["количество_договоров"].str.strip() != "-") &
+                                     (self.table["код_инструмента"].str.strip() not in ["Итого:", "Итого по секции:"])]
