ed296c5d93737a8deef00125620a7b40d67762fe
Anton Gromov <seed14112@yandex.ru>
2025-04-04 10:39:20 +0300

    [~] Изменение convert_date
    R: Не было выбора окончания даты и валидации корректности дат
    FB: Добавлена валидация и добавлен ввод даты начала и окончания

--- func.py
+++ func.py
@@ -18,12 +18,14 @@ def convert_date():
         try:
             start_date, end_date = tuple(map(lambda x: datetime.datetime.strptime(x, "%d.%m.%Y").date(),
                                              (start_date_input, end_date_input)))
-            
+
             if start_date > end_date or end_date > datetime.date.today():
                 print("Некорректный диапазон дат")
                 continue
+
             else:
                 return start_date, end_date
+            
         except ValueError:
             print("Некорректный формат даты: Введите дату в формате: ДД.ММ.ГГГГ")
 
