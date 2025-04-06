e1098d429b9990c16319467b918100bb38c5f9c4
Anton Gromov <seed14112@yandex.ru>
2025-04-06 18:27:40 +0300

    [+] Написание входной функции для запуска асинхронного приложения

--- async_main.py
+++ async_main.py
@@ -1,8 +1,10 @@
+import asyncio
 import datetime
 import logging
 
+from config import MAX_CONCURRENT_TASKS
 from db.database import DataBase
-from func import convert_date
+from func import convert_date, gen_date
 from services.data_transformation import DataTransformation
 from services.excel_parsers import ExcelParser
 from services.load_tables import LoadTable
@@ -14,34 +16,58 @@ logging.basicConfig(
 )
 
 db = DataBase()
-uow = UnitOfWork(db.async_session())
+semaphore = asyncio.Semaphore(int(MAX_CONCURRENT_TASKS))  # Ограничение на количество одновременных запросов
+
+
+async def process_single_date(loader: LoadTable, date: datetime.datetime):
+    """
+    Обрабатывает одну дату и сохраняет в БД.
+
+    Args:
+        loader (LoadTable): Объект для загрузки данных из файла.
+        date (datetime.datetime): Дата для обработки.
+    """
+    async with semaphore:
+        uow = UnitOfWork(db.async_session())
+        table_info = await loader.async_load()
+        if not table_info:
+            logging.info(f"Нет данных за {date.strftime('%d.%m.%Y')} г.")
+            return  # Если нет данных, пропускаем эту дату
+
+        parser = ExcelParser(table_info)
+        table = parser.table
+        transfer = DataTransformation(table, date)
+        transfer_data_for_db = transfer.transform()
+
+        async with uow.async_start() as session:
+            session.trading_results.add_all(transfer_data_for_db)
+            logging.info(f"Загрузка информации в БД за {date.strftime("%d.%m.%Y")} г.")
 
 
 async def async_main():
     """Главная функция запуска aсинхронного приложения."""
     start_date, end_date = convert_date()
+    date_generator = gen_date(start_date)
     logging.info(f"Начало работы асинхронного приложения {datetime.datetime.now()}")
     time_now = datetime.datetime.now()
     await db.async_create_db()
-    loader = LoadTable(start_date, end_date)
-    try:
-        while True:
-            table_info = await loader.async_load()
-            table_date = loader.table_date
-            if not table_info:
-                logging.info(f"Нет данных за {table_date.strftime('%d.%m.%Y')} г.")
-                continue
-            parser = ExcelParser(table_info)
-            table = parser.table
-            transfer = DataTransformation(table, table_date)
-            transfer_data_for_db = transfer.transform()
-            async with uow.async_start() as session:
-                await session.trading_results.add_all(transfer_data_for_db)
-                logging.info(f"Загрузка информации в БД за {table_date.strftime("%d.%m.%Y")} г.")
-    except StopIteration:
-        logging.info("Парсинг завершен")
-        logging.info(f"Время работы приложения:{datetime.datetime.now() - time_now}")  # Время работы
+
+    tasks = []
+
+    while True:
+        date = next(date_generator)  # Получаем следующую дату
+        if date > end_date:  # Проверяем, не достигли ли мы конечной даты
+            break
+
+        loader = LoadTable(date)
+        task = asyncio.create_task(process_single_date(loader, date))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+    logging.info("Парсинг завершен")
+    logging.info(f"Время работы приложения:{datetime.datetime.now() - time_now}")
 
 
 if __name__ == "__main__":
-    async_main()
+    asyncio.run(async_main())
