98102a6dd377d72a46750fb2f157aea7e1c5f8a2
Anton Gromov <seed14112@yandex.ru>
2025-04-06 18:26:04 +0300

    [~] Изменение функции sync_main
    R: Код не работал в связи с изменением класса LoadTable
    
    FB: Рефактор кода в связи с изменением класса LoadTable

--- sync_main.py
+++ sync_main.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 
 from db.database import DataBase
-from func import convert_date
+from func import convert_date, gen_date
 from services.data_transformation import DataTransformation
 from services.excel_parsers import ExcelParser
 from services.load_tables import LoadTable
@@ -20,27 +20,31 @@ uow = UnitOfWork(db.sync_session())
 def sync_main():
     """Главная функция запуска синхронного приложения."""
     start_date, end_date = convert_date()
+    date_generator = gen_date(start_date)
     logging.info(f"Начало работы синхронного приложения {datetime.datetime.now()}")
     time_now = datetime.datetime.now()
     db.sync_create_db()
-    loader = LoadTable(start_date, end_date)
-    try:
-        while True:
-            table_info = loader.sync_load()
-            table_date = loader.table_date
-            if not table_info:
-                logging.info(f"Нет данных за {table_date.strftime('%d.%m.%Y')} г.")
-                continue
-            parser = ExcelParser(table_info)
-            table = parser.table
-            transfer = DataTransformation(table, table_date)
-            transfer_data_for_db = transfer.transform()
-            with uow.sync_start() as session:
-                session.trading_results.add_all(transfer_data_for_db)
-                logging.info(f"Загрузка информации в БД за {table_date.strftime("%d.%m.%Y")} г.")
-    except StopIteration:
-        logging.info("Парсинг завершен")
-        logging.info(f"Время работы приложения:{datetime.datetime.now() - time_now}")  # Время работы
+
+    while True:
+        date = next(date_generator)  # Получаем следующую дату
+        if date > end_date:  # Проверяем, не достигли ли мы конечной даты
+            break
+        loader = LoadTable(date)
+
+        table_info = loader.sync_load()
+        if not table_info:
+            logging.info(f"Нет данных за {date.strftime('%d.%m.%Y')} г.")
+            continue
+        parser = ExcelParser(table_info)
+        table = parser.table
+        transfer = DataTransformation(table, date)
+        transfer_data_for_db = transfer.transform()
+        with uow.sync_start() as session:
+            session.trading_results.add_all(transfer_data_for_db)
+            logging.info(f"Загрузка информации в БД за {date.strftime("%d.%m.%Y")} г.")
+
+    logging.info("Парсинг завершен")
+    logging.info(f"Время работы приложения:{datetime.datetime.now() - time_now}")  # Время работы
 
 
 if __name__ == "__main__":
