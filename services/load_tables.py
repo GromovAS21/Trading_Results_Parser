7ab351aef0353a5cbe4abaa33e1e0099681b2758
Anton Gromov <seed14112@yandex.ru>
2025-04-06 18:22:19 +0300

    [~] Переписывание логики класса LoadTable
    R: Некорректно работал генератор в классе при большом количестве сессий
    
    FB: Убран метод генератор и переписана логика работы класса

--- services/load_tables.py
+++ services/load_tables.py
@@ -1,6 +1,6 @@
 import datetime
 from pathlib import Path
-from typing import Generator, Optional
+from typing import Optional
 
 import aiohttp
 import requests
@@ -10,19 +10,17 @@ class LoadTable:
     """Класс для загрузки Exel таблицы c сайта spimex.com."""
 
     SITE_URL = "https://spimex.com/upload/reports/oil_xls/"
+    COUNTER_END_DATE = True
 
-    def __init__(self, start_date, end_date):
+    def __init__(self, current_date: datetime.datetime):
         """
         Инициализация класса.
 
         Args:
-            start_date (datetime.datetime): Начальная дата
-            end_date (datetime.datetime): Конечная дата
+            current_date (datetime.datetime): Текущая дата
         """
-        self._start_date: datetime.datetime = start_date
-        self._end_date: datetime.datetime = end_date
+        self._current_date: datetime.datetime = current_date
         self._table_date: Optional[datetime.datetime] = None
-        self._date_generator: Generator = self._gen_date()
         self._path_file: Optional[Path] = None
 
     @property
@@ -35,28 +33,6 @@ class LoadTable:
         """
         return self._path_file
 
-    @property
-    def table_date(self) -> datetime.datetime:
-        """
-        Получение текущей даты.
-
-        Returns:
-            datetime.datetime: Текущая дата
-        """
-        return self._table_date
-
-    def _gen_date(self) -> Generator:
-        """
-        Генератор дат указанного диапазона.
-
-        Returns:
-            datetime.datetime: Дата для загрузки файла
-        """
-        current_date = self._start_date
-        while current_date < self._end_date:
-            yield current_date
-            current_date += datetime.timedelta(days=1)
-
     def get_filename(self) -> str:
         """
         Формирует имя файла по заданной дате.
@@ -65,8 +41,7 @@ class LoadTable:
             str: Имя файла
         """
         filename = lambda x: "oil_xls_{}162000.xls".format(x.strftime("%Y%m%d"))
-        self._table_date = next(self._date_generator)
-        return filename(self._table_date)
+        return filename(self._current_date)
 
     def sync_load(self) -> bytes:
         """Синхронно загружает файл по указанному адресу."""
